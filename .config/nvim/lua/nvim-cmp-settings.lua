cb80ad21c770e3680d42215c35eb5335ee4473e2
Brian Malinconico <bmalinconico@terminus.com>
2021-11-05 20:52:15 -0400

    Adding new completion options

--- .config/nvim/lua/nvim-cmp-settings.lua
+++ .config/nvim/lua/nvim-cmp-settings.lua
@@ -1,15 +1,19 @@
 local cmp = require'cmp'
 cmp.setup({
-  -- mapping = {
-  --   ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  -- },
+  experimental = {
+    native_menu = false,
+    ghost_text = true,
+  },
+  mapping = {
+    ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' })
+  },
   buffer = {
     get_bufnrs = function()
       return vim.api.nvim_list_bufs()
     end
   },
   sources = {
-    { name = 'nvim_lsp' },
+    { name = 'nvim_lsp', max_item_count = 10 },
     { name = 'buffer' },
     { name = 'path' },
   }
